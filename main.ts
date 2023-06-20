65a6f1664436d3194e74f68e9f1363097cafcd66
Jackasaurus <137194798+Jackasaurus@users.noreply.github.com>
2023-06-20 16:02:47 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,88 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("NA NA")
+})
+input.onButtonPressed(Button.AB, function () {
+    music.play(music.stringPlayable("C5 A B G A F G E ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # # . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # # # .
+        # # # # .
+        # # # # .
+        # # # # .
+        # # # # .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("Jack")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+})
